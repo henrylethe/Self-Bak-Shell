a4621a58952ab34e29b383f579a0503a01946355
Henry Zhu <henrylethe@users.noreply.github.com>
2017-07-17 20:29:30 +0800

    Update fordan.sh

--- fordan.sh
+++ fordan.sh
@@ -8,6 +8,7 @@
 #
 
 # Download and unzip files
+echo 'Downloading and unzipping…'
 cd ~/Downloads
 curl 'http://archive.apache.org/dist/apr/apr-1.6.2.tar.bz2' > ~/Downloads/apr-1.6.2.tar.bz2
 bunzip2 apr-1.6.2.tar.bz2
@@ -23,53 +24,64 @@ bunzip2 httpd-2.4.27.tar.bz2
 tar -xf httpd-2.4.27.tar
 
 # Install dependency
+clear
+echo 'Installing apr…'
 cd ~/Downloads/apr-1.6.2/
 ./configure --prefix=/usr/local/apr
 sudo make
 sudo make install
 
+clear
+echo 'Installing apr-util…'
 cd ~/Downloads/apr-util-1.6.0/
 ./configure --prefix=/usr/local/apr-util --with-apr=/usr/local/apr/
 sudo make
 sudo make install
 
+clear
+echo 'Installing pcre…'
 cd ~/Downloads/pcre-8.41/
 ./configure --prefix=/usr/local/pcre --with-apr=/usr/local/apr/
 sudo make
 sudo make install
 
+clear
+echo 'Installing httpd…'
 cd ~/Downloads/httpd-2.4.27/
 ./configure --prefix=/usr/local/httpd --with-pcre=/usr/local/pcre --with-apr=/usr/local/apr --with-apr-util=/usr/local/apr-util
 sudo make
 sudo make install
 
 # Build mods
+clear
+echo 'Compiling mod_dav_svn & mod_authz_svn…'
 cd ~/Downloads/subversion-1.7.19/
 ./configure --prefix=/Applications/Xcode.app/Contents/Developer/usr --disable-debug --with-zlib=/usr --disable-mod-activation --with-apache-libexecdir=$(/usr/local/httpd/bin/apxs -q libexecdir) --without-berkeley-db --disable-nls --without-serf --with-apr=/usr/local/apr --with-apr-util=/usr/local/apr-util --with-apxs="/usr/local/httpd/bin/apxs"
 make mod_dav_svn mod_authz_svn
 cd ~/Downloads
 
 #  Dump used libs
+clear
 otool -L "subversion-1.7.19/subversion/mod_dav_svn/.libs/mod_dav_svn.so"
 otool -L "subversion-1.7.19/subversion/mod_authz_svn/.libs/mod_authz_svn.so"
 
 #  Clean up
 echo 'Copying modules to current directory…'
-mv "subversion-1.7.19/subversion/mod_dav_svn/.libs/mod_dav_svn.so" './mod_dav_svn.so'
-mv "subversion-1.7.19/subversion/mod_authz_svn/.libs/mod_authz_svn.so" './mod_authz_svn.so'
+sudo mv "subversion-1.7.19/subversion/mod_dav_svn/.libs/mod_dav_svn.so" './mod_dav_svn.so'
+sudo mv "subversion-1.7.19/subversion/mod_authz_svn/.libs/mod_authz_svn.so" './mod_authz_svn.so'
 
 echo 'Deleting temporary files…'
-rm -rf "apr-1.6.2"
-rm -f "apr-1.6.2.tar.bz2"
-rm -rf "apr-util-1.6.0"
-rm -f "apr-util-1.6.0.tar.bz2"
-rm -rf "pcre-8.41"
-rm -f "pcre-8.41.tar.bz2"
-rm -rf "httpd-2.4.27"
-rm -f "httpd-2.4.27.tar.bz2"
+sudo rm -rf "apr-1.6.2"
+sudo rm -f "apr-1.6.2.tar.bz2"
+sudo rm -rf "apr-util-1.6.0"
+sudo rm -f "apr-util-1.6.0.tar.bz2"
+sudo rm -rf "pcre-8.41"
+sudo rm -f "pcre-8.41.tar.bz2"
+sudo rm -rf "httpd-2.4.27"
+sudo rm -f "httpd-2.4.27.tar.bz2"
 
-rm -rf "subversion-1.7.19"
-rm -f "subversion-1.7.19.tar.bz2"
+sudo rm -rf "subversion-1.7.19"
+sudo rm -f "subversion-1.7.19.tar.bz2"
 
 printf '\n\nAll done!\n'
 
